65a0cc64be4f5b4b9b55645563014b4584eec5d8
Amohelang Mohlomi <ammohlojhb024@student.wethinkcode.co.za>
2025-09-30 11:29:51 +0200

    Added required methods and variables in the Account class

--- src/main/java/Account.java
+++ src/main/java/Account.java
@@ -1,2 +1,27 @@
 public abstract class Account {
+    protected String accountNumber;
+
+
+
+    protected int balance;
+
+    abstract  void displayInfo();
+
+    public void deposit(int amountInCents){
+
+    }
+
+    public void withdraw(int amountInCents){
+
+    }
+
+    public String getAccountNumber() {
+        return accountNumber;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+
 }
