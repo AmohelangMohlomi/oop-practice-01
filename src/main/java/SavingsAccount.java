882aa4b5093d5bf5b831b64a3c6e1687c997a8e7
Amohelang Mohlomi <ammohlojhb024@student.wethinkcode.co.za>
2025-09-30 11:43:58 +0200

    Created Account constructor for Savings Accounty

--- src/main/java/SavingsAccount.java
+++ src/main/java/SavingsAccount.java
@@ -1,6 +1,10 @@
 public class SavingsAccount extends Account{
 
 
+    public SavingsAccount(String accountNumber) {
+        super(accountNumber);
+    }
+
     @Override
     void displayInfo() {
 
