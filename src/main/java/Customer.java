dd8c65c132e0ba4b42efa0a8e064244358e12c44
Amohelang Mohlomi <ammohlojhb024@student.wethinkcode.co.za>
2025-09-30 11:33:47 +0200

    Added fields and methods for the Customer class

--- src/main/java/Customer.java
+++ src/main/java/Customer.java
@@ -1,2 +1,27 @@
+import java.util.ArrayList;
+
 public class Customer {
+
+
+    public String name;
+    public String customerId;
+    public ArrayList<Account> accounts;
+
+    public void addAccount(Account account){
+
+    }
+
+
+    public String getName() {
+        return name;
+    }
+
+    public String getCustomerId() {
+        return customerId;
+    }
+
+    public ArrayList<Account> getAccounts() {
+        return accounts;
+    }
+
 }
